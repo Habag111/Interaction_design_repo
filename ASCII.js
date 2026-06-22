80bbc863f500845f887b197fadaf821665ba7aca
Jan Darski <spam.eimail@gmail.com>
2026-06-22 23:01:35 +0200

    inverted colors to match the rest of the scenes, fills the whole screen. Might have some stretching? idk

--- ASCII.js
+++ ASCII.js
@@ -41,9 +41,6 @@ const inputString = 'DA:51:70:53'
 const arrangedDensity = buildDensityString(inputString, density, density_end)
 console.log(arrangedDensity)
 let video
-let asciiDiv;
-
-
 
 function ascii_setup() {
   ascii_cleanup();
@@ -51,13 +48,7 @@ function ascii_setup() {
   background(0);
   video = createCapture(VIDEO);
   video.size(120,90);
-  asciiDiv = createDiv();
-  asciiDiv.style('position', 'fixed');
-  asciiDiv.style('inset', '0');
-  asciiDiv.style('z-index', '10');
-  asciiDiv.style('pointer-events', 'none');
   video.hide();
-  
 }
 
 function ascii_cleanup() {
@@ -65,35 +56,38 @@ function ascii_cleanup() {
     video.remove();
     video = null;
   }
+}
 
-  if (asciiDiv) {
-    asciiDiv.remove();
-    asciiDiv = null;
-  }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 
-  function ascii_stage(){
-
+function ascii_stage(){
   background(0);
   video.loadPixels();
-    let asciiImage= '';
-  for (let j=0; j < video.height; j++) { 
-  for (let i= 0; i < video.width; i++) {
-  const pixelIndex = (i+j*video.width)*4;
-  const r = video.pixels[pixelIndex +0];
-  const g = video.pixels[pixelIndex +1];
-  const b = video.pixels[pixelIndex +2];
-  const avg = (r+g+b)/ 3;
-  const len = arrangedDensity.length;
-  const charIndex = floor(map(avg,0,255,0, len));
+  const cellWidth = width / video.width;
+  const cellHeight = height / video.height;
+  const textSizeValue = Math.max(8, Math.floor(Math.min(cellWidth, cellHeight)));
 
-  const c = arrangedDensity.charAt(charIndex) 
-  if (c ==' ') asciiImage +='&nbsp;'
- else asciiImage += c;
+  textFont('Courier');
+  textAlign(LEFT, TOP);
+  textSize(textSizeValue);
+  noStroke();
+  fill(255);
+
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = arrangedDensity.length;
+      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const c = arrangedDensity.charAt(charIndex);
+
+      text(c, i * cellWidth, j * cellHeight);
+    }
   }
-    asciiImage += '<br/>';
-
-  } 
-  asciiDiv.html(asciiImage);
 }
